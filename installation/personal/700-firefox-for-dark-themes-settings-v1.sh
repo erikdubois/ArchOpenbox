e80346f1a9af3267c801abd7d333c2d933225f19
Erik Dubois <erik.dubois@gmail.com>
2017-02-07 12:34:47 +0100

    Comment :  on Tue Feb  7 12:34:47 CET 2017

--- installation/personal/700-firefox-for-dark-themes-settings-v1.sh
+++ installation/personal/700-firefox-for-dark-themes-settings-v1.sh
@@ -14,7 +14,6 @@ set -e
 
 
 echo "Making sure firefox looks great in dark themes"
-echo "You should run this script after you rebooted and are in i3."
 echo "Firefox must have started once. The directory will not exist otherwise."
 
 cp -r settings/firefox/chrome/ ~/.mozilla/firefox/*.default
