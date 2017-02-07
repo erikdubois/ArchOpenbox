7950a3f972637a83ebeca1e9b42f9f286de091e4
Erik Dubois <erik.dubois@gmail.com>
2017-02-07 16:00:50 +0100

    Comment :  on Tue Feb  7 16:00:50 CET 2017

--- installation/400-install-software-distro-specific-v1.sh
+++ installation/400-install-software-distro-specific-v1.sh
@@ -450,45 +450,6 @@ else
 fi
 
 
-echo "################################################################"
-echo "geany"   
-echo "################################################################"
-
-
-package="geany"
-
-#----------------------------------------------------------------------------------
-
-#checking if application is already installed or else install with aur helpers
-if pacman -Qi $package &> /dev/null; then
-
-	echo "################################################################"
-	echo "################## "$package" is already installed"
-	echo "################################################################"
-
-else
-
-	#checking which helper is installed
-	if pacman -Qi packer &> /dev/null; then
-
-		echo "Installing with packer"
-		packer -S --noconfirm --noedit  $package
-
-	elif pacman -Qi pacaur &> /dev/null; then
-		
-		echo "Installing with pacaur"
-		pacaur -S --noconfirm --noedit  $package
-		 	
-	elif pacman -Qi yaourt &> /dev/null; then
-
-		echo "Installing with yaourt"
-		yaourt -S --noconfirm $package
-			  	
-	fi
-
-
-fi
-
 
 
 echo "################################################################"
