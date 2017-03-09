52791e64890a236c122037ccdd6769025d9a0695
Erik Dubois <erik.dubois@gmail.com>
2017-03-09 07:42:33 +0100

    Comment :  on Thu Mar  9 07:42:33 CET 2017

--- installation/personal/600-install-personal-settings-folders-v1.sh
+++ installation/personal/600-install-personal-settings-folders-v1.sh
@@ -46,7 +46,6 @@ echo "################################################################"
 [ -d $HOME"/Music" ] || mkdir -p $HOME"/Music"
 [ -d $HOME"/Pictures" ] || mkdir -p $HOME"/Pictures"
 [ -d $HOME"/Videos" ] || mkdir -p $HOME"/Videos"
-[ -d $HOME"/Screenshots" ] || mkdir -p $HOME"/Screenshots"
 
 
 echo "################################################################"
